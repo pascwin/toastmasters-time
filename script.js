6555a5363ae7dbbe764173d72b27bc0ec20f2b59
pascwin <winner@hm.edu>
2020-09-23 22:48:37 +0200

    Namen der Farben hinzugefügt

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const startBtn = document.getElementById("start");
 const body = document.getElementById("body")
+const start = document.getElementById("start");
+const green = document.getElementById("green");
+const yellow = document.getElementById("yellow");
+const red = document.getElementById("red");
 
 console.log(body)
 
@@ -7,17 +10,24 @@ const changeColor = () => {
     if(body.className === "grey") {
         body.classList.remove("grey")
         body.classList.add("green")
-        startBtn.hidden;
+        start.hidden = true;
+        green.hidden = false;
     } else if (body.className === "green"){
         body.classList.remove("green")
         body.classList.add("yellow")
+        green.hidden = true;
+        yellow.hidden = false;
     } else if (body.className === "yellow"){
         body.classList.remove("yellow")
         body.classList.add("red")
+        yellow.hidden = true;
+        red.hidden = false;
     } else if (body.className === "red") {
         body.classList.remove("red")
         body.classList.add("grey")
+        red.hidden = true;
+        start.hidden = false;
     }
 }
-console.log(startBtn)
+
 body.addEventListener("click", changeColor)
